e1baefafde8eefe1251ea6e9fd7cc9a25d76daf0
Sebastian Ponce <poncegramming@gmail.com>
2024-06-06 03:26:17 +0000

    fix about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,7 +4,7 @@ export function About() {
   return (
     <div className="flex flex-col min-h-[100dvh]">
       <header className="px-4 lg:px-6 h-14 flex items-center">
-        <Link href="#" className="flex items-center justify-center" prefetch={false}>
+        <Link href="/" className="flex items-center justify-center" prefetch={false}>
           <WaypointsIcon className="h-6 w-6" />
           <span className="sr-only">Walkify</span>
         </Link>
